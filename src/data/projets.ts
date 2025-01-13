ad77276b0a8fcd491f95b70015064f87f3b4a64b
hichamBen87 <hicham.benjabrou1@gmail.com>
2025-01-13 17:33:59 +0100

    second commit

--- src/data/projets.ts
+++ src/data/projets.ts
@@ -11,7 +11,7 @@ export const projets = [
     id: 1,
     titre: "Amazon Clon App",
     description: "Une application e-commerce moderne, inspirée d'Amazon, avec navigation fluide, panier interactif, gestion des produits, avis clients, et filtres avancés, offrant une expérience utilisateur optimisée et évolutive.",
-    technologies: ["React-TypeScript", "Tailwind Css", "HTML5"],
+    technologies: ["React-TypeScript", "Tailwind Css", "HTML5","Node js"],
     image: "/images/amazon.PNG",  // Image du projet
     lien: "https://drive.google.com/file/d/1fvClPBIVdoQq43G1e69tFb4mB06iaGbe/view?usp=drive_link"  // Lien vers la vidéo sur Google Drive
   }
